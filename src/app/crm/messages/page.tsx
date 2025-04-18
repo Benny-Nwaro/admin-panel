8a12958a7ad5d34a635904176f4b90c41f961e35
Benny-Nwaro <baroh3767@aol.com>
2025-04-18 17:25:58 +0100

    update

--- src/app/crm/messages/page.tsx
+++ src/app/crm/messages/page.tsx
@@ -1,17 +1,15 @@
 "use client"
 import React from 'react';
 import MainWrapper from '@/components/MainWarpper';
-import MessageList from '@/components/messages/AllMessages';
+import ChatLayout from '@/components/crm/messages/ChatLayout';
 
 const Message: React.FC = () => {
 
     return (
         <MainWrapper>
-        <div className="flex flex-col lg:flex-row gap-4 pt-10 lg:px-10 ">
-            <div className="flex-1 bg-white shadow-lg rounded-lg p-4">
-                <MessageList />
+            <div className="flex-1 bg-white shadow-lg mx-16 h-full rounded-lg p-4">
+                <ChatLayout />
             </div>
-        </div>
     </MainWrapper>
     );
 };
